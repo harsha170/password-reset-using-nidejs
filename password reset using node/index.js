9b5a422db612d390dece05e40cf1d97c4c705b7a
Kurumindla Harshavardhan <55336252+harsha170@users.noreply.github.com>
2020-12-26 09:14:50 +0530

    Update index.js

--- password reset using node/index.js
+++ password reset using node/index.js
@@ -10,7 +10,7 @@ const objectId = mongodb.ObjectID;
 
 const app = express();
 const dbURL = "mongodb+srv://harsha:8tt65gWx8bWptluV@password-recovery.a6ytp.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
